0d6ecab5ffa6bca73b4ebc8c8782cc176a614262
Natalka <ntyszkiewicz@gmail.com>
2015-09-01 18:39:01 +0200

    typo

--- spec/models/media_item_spec.rb
+++ spec/models/media_item_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe MediaItem do
   it 'should not be published be default' do
-    expect(User.new).to_not be_published
+    expect(MediaItem.new).to_not be_published
   end
 
   it { should validate_presence_of :source_link }
