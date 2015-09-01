9b2ab84c92aec4084166edc05a956e9f88c463aa
Natalka <ntyszkiewicz@gmail.com>
2015-09-01 19:27:10 +0200

    validate MediaItems youtube link

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,3 +1,5 @@
 class MediaItem < ActiveRecord::Base
-  validates :source_link, presence: true
+  YOUTUBE_REGEX = /\A(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})\z/
+  validates :source_link, presence: true, format: { with: YOUTUBE_REGEX }
+
 end
--- spec/models/media_item_spec.rb
+++ spec/models/media_item_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
 
 describe MediaItem do
+  let(:media_item) { FactoryGirl.build(:media_item) }
+
+
   it 'should not be published be default' do
     expect(MediaItem.new).to_not be_published
   end
 
   it { should validate_presence_of :source_link }
+
+  context '#validates YouTube url' do
+    let(:media_item_domain) { FactoryGirl.build(:media_item_wrong_domain) }
+    let(:media_item_uri) { FactoryGirl.build(:media_item_wrong_uri) }
+
+    it 'should be valid for correct youtube link' do
+      expect(media_item).to be_valid
+    end
+
+    it 'should not be valid incorrect domain link' do
+      expect(media_item_domain).to_not be_valid
+    end
+
+    it 'should not be valid incorrect params' do
+      expect(media_item_uri).to_not be_valid
+    end
+  end
+
 end
